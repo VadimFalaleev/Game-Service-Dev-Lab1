baa0ef2bfa6828e50cedc13a9be35141e751be76
Вадим <54228342+VadimFalaleev@users.noreply.github.com>
2022-09-23 10:30:59 +0500

    Task 3

--- Game Service Dev LAB1/Assets/Scripts/SpawnCubesScript.cs
+++ Game Service Dev LAB1/Assets/Scripts/SpawnCubesScript.cs
@@ -7,21 +7,43 @@ public class SpawnCubesScript : MonoBehaviour
     [SerializeField] public GameObject panel;
     [SerializeField] public GameObject cube;
     [SerializeField] InputField Field;
-    [SerializeField] public List<Transform> spawnPoints= new List<Transform>();
+    [SerializeField] public List<Vector3> spawnPoints = new List<Vector3>();
+    [SerializeField] public GameObject cubePointMin;
+    [SerializeField] public GameObject cubePointMax;
 
-    public void Start()
+    public void Awake()
     {
-        Time.timeScale = 0;
+        if(panel.activeSelf)
+            Time.timeScale = 0;
     }
 
-    private void GenerateButton()
+    public void GenerateButton()
     {
+        GetPosition();
+
         panel.SetActive(false);
         Time.timeScale = 1;
 
-        for(int i = 0; i < int.Parse(Field.text); i++)
+        for (int i = 0; i < int.Parse(Field.text); i++)
+            Instantiate(cube, spawnPoints[i], cube.transform.rotation);
+    }
+
+    private void GetPosition()
+    {
+        float cubePointMinX = cubePointMin.transform.position.x;
+        float cubePointMinZ = cubePointMin.transform.position.z;
+        float cubePointMaxX = cubePointMax.transform.position.x;
+        float cubePointMaxZ = cubePointMax.transform.position.z;
+
+        for (int i = 0; i < int.Parse(Field.text); i++)
         {
-            
+            Vector3 newPoint = new Vector3(
+                Random.Range(cubePointMinX, cubePointMaxX),
+                cubePointMax.transform.position.y,
+                Random.Range(cubePointMinZ, cubePointMaxZ));
+
+            if (!spawnPoints.Contains(newPoint))
+                spawnPoints.Add(newPoint);
         }
     }
 }
