9c58b1f2a0d0e4f26e94fb0e504746222b378930
Вадим <54228342+VadimFalaleev@users.noreply.github.com>
2022-09-23 13:16:51 +0500

    fix

--- Game Service Dev LAB1/Assets/Scripts/SpawnCubesScript.cs
+++ Game Service Dev LAB1/Assets/Scripts/SpawnCubesScript.cs
@@ -4,28 +4,27 @@ using UnityEngine.UI;
 
 public class SpawnCubesScript : MonoBehaviour
 {
-    [SerializeField] public GameObject panel;
-    [SerializeField] public GameObject cube;
-    [SerializeField] InputField Field;
-    [SerializeField] public List<Vector3> spawnPoints = new List<Vector3>();
-    [SerializeField] public GameObject cubePointMin;
-    [SerializeField] public GameObject cubePointMax;
+    [SerializeField] GameObject panel; // Определяем объект Panel на сцене.
+    [SerializeField] InputField Field; // Определяем строку ввода на сцене.
+    [SerializeField] GameObject cube; // Определяем объект, который будем генерировать.
+    [SerializeField] GameObject cubePointMin; 
+    [SerializeField] GameObject cubePointMax; // Определяем объекты, служащие границей для генерации объектов.
+    [SerializeField] List<Vector3> spawnPoints = new(); // Определяем список, где будут хранится координаты, куда могут генерироваться объекты.
 
     public void Awake()
     {
-        if(panel.activeSelf)
-            Time.timeScale = 0;
+        Time.timeScale = 0; // При запуске проект будет на паузе.
     }
 
-    public void GenerateButton()
+    public void GenerateButton() // Этот метод бдует работать после нажатия кнопки.
     {
         GetPosition();
 
-        panel.SetActive(false);
-        Time.timeScale = 1;
+        panel.SetActive(false); // Закрываем панель.
+        Time.timeScale = 1; // Убираем проект с паузы.
 
         for (int i = 0; i < int.Parse(Field.text); i++)
-            Instantiate(cube, spawnPoints[i], cube.transform.rotation);
+            Instantiate(cube, spawnPoints[i], cube.transform.rotation); // Генерируем нужно количество объектов.
     }
 
     private void GetPosition()
@@ -33,17 +32,17 @@ public class SpawnCubesScript : MonoBehaviour
         float cubePointMinX = cubePointMin.transform.position.x;
         float cubePointMinZ = cubePointMin.transform.position.z;
         float cubePointMaxX = cubePointMax.transform.position.x;
-        float cubePointMaxZ = cubePointMax.transform.position.z;
+        float cubePointMaxZ = cubePointMax.transform.position.z; // Записываем координаты Х и Z границ для удобства.
 
         for (int i = 0; i < int.Parse(Field.text); i++)
         {
-            Vector3 newPoint = new Vector3(
+            Vector3 newPoint = new(
                 Random.Range(cubePointMinX, cubePointMaxX),
                 cubePointMax.transform.position.y,
-                Random.Range(cubePointMinZ, cubePointMaxZ));
+                Random.Range(cubePointMinZ, cubePointMaxZ)); // Определяем точку, куда будем генерировать объект.
 
             if (!spawnPoints.Contains(newPoint))
-                spawnPoints.Add(newPoint);
+                spawnPoints.Add(newPoint);  // Добавляем точку в список, если такой еще нет.
         }
     }
 }
